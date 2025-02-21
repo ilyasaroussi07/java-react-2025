a3a76283f3593c2fbf518d825cfad6167434f9d8
ilyasaroussi07 <ilyasaroussi07@gmail.com>
2025-02-21 13:35:33 +0100

    Create ComentarioVariasLineas.java

---
+++ C2-JavaBasic/src/ComentarioVariasLineas.java
@@ -0,0 +1,11 @@
+
+public class ComentarioVariasLineas {
+
+	public static void main(String args[] ) {
+		// TODO Auto-generated method stub
+		for(int i=0 ; i<6 ; i++ )
+			System.out.print(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
