20d9683aaa7a66d2b5c603c736135e61a1303b26
ilyasaroussi07 <ilyasaroussi07@gmail.com>
2025-06-16 10:22:34 +0200

    update

--- C2-JavaBasic/src/UD09/T9Ej2Entregable.java
+++ C2-JavaBasic/src/UD09/T9Ej2Entregable.java
@@ -1,5 +1,10 @@
 package UD09;
 
 public interface T9Ej2Entregable {
-
-}
+	public interface T9Ej2Entregable {
+		void entregar();
+	    void devolver();
+	    boolean isEntregado();
+	    int compareTo(Object a);
+	    
+	}
